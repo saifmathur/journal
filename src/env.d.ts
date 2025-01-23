c904b7bc529d5b797f16488183b42fc189d52d5e
saifmathur <saifmathur41@gmail.com>
2025-01-23 17:45:19 +0530

    ngx web builder

---
+++ src/env.d.ts
@@ -0,0 +1,17 @@
+// Define the type of the environment variables.
+declare interface Env {
+  readonly NODE_ENV: string;
+  // Replace the following with your own environment variables.
+  // Example: NGX_VERSION: string;
+  [key: string]: any;
+}
+
+declare interface ImportMeta {
+  readonly env: Env;
+}
+
+// You can modify the name of the variable in angular.json.
+// ngxEnv: {
+//  define: '_NGX_ENV_',
+// }
+declare const _NGX_ENV_: Env;
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 
 export const environment = {
   production: true,
-  apiUrl: process.env['API_URL'] || 'https://journal-service-wurr.onrender.com',
+  apiUrl: import.meta.env["API_URL"] || 'https://journal-service-wurr.onrender.com',
   server_username: 'user',
   server_password: 'saif',
   //apiKey: process.env['API_KEY'] || '',
