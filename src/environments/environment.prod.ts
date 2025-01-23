e0b3e174b723275731a2301aa85d7bef1ad37903
saifmathur <saifmathur41@gmail.com>
2025-01-23 16:24:39 +0530

    try

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 
 export const environment = {
   production: true,
-  apiUrl: process.env["API_URL"] || '',
+  apiUrl: 'API_URL',
   server_username: 'user',
   server_password: 'saif',
   //apiKey: process.env['API_KEY'] || '',
