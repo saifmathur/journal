77d12fbac8724f12dd361f18056bb432ea63df15
saifmathur <saifmathur41@gmail.com>
2025-02-25 22:19:31 +0530

    check 2

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,8 +2,8 @@
 export const environment = {
   production: true,
   // apiUrl: 'https://journal-service-wurr.onrender.com',
-  apiUrl: 'https://journal-taupe-theta.vercel.app',
-  hostName: 'journal-taupe-theta.vercel.app',
+  apiUrl: 'https://journal-service-production.up.railway.app',
+  hostName: 'journal-service-production.up.railway.app',
   server_username: 'user',
   server_password: 'saif',
   //apiKey: process.env['API_KEY'] || '',
