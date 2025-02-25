3bb37abf6641c6cd4333acb464562f59eec7b3f8
saifmathur <saifmathur41@gmail.com>
2025-02-25 21:55:34 +0530

    connect to railway

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,9 @@
 
 export const environment = {
   production: true,
-  apiUrl: 'https://journal-service-wurr.onrender.com',
-  hostName: 'journal-service-wurr.onrender.com',
+  // apiUrl: 'https://journal-service-wurr.onrender.com',
+  apiUrl: 'journal-service.railway.internal',
+  hostName: 'journal-service.railway.internal',
   server_username: 'user',
   server_password: 'saif',
   //apiKey: process.env['API_KEY'] || '',
