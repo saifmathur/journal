3eeee1817d8d49c0919f78c79d308efa352ec88d
saifmathur <saifmathur41@gmail.com>
2025-04-27 16:57:09 +0530

    switched to render

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 
 export const environment = {
   production: true,
-  // apiUrl: 'https://journal-service-wurr.onrender.com',
-  apiUrl: 'https://journal-service-production.up.railway.app',
+  apiUrl: 'https://journal-service-wurr.onrender.com',
+  // apiUrl: 'https://journal-service-production.up.railway.app',
   hostName: 'journal-service-production.up.railway.app',
   server_username: 'user',
   server_password: 'saif',
